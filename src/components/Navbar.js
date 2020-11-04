aa296b3cd117497cce7da4b6596f479291fb7019
rouf mohamad <ratherrouf678@gmail.com>
2020-11-04 19:23:11 +0530

    Accessing Context by using Consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
+// 1)Accessing Context using contextType and this.context
+/* --- 
 class Navbar extends React.Component{
     static contextType = ThemeContext;
     render(){
-        console.log(this.context); // Now this.context contains all our app-data
         const {isLightTheme, dark, light} = this.context;
         const theme = isLightTheme ? light : dark;
         return (
@@ -12,21 +13,40 @@ class Navbar extends React.Component{
                 <h3>BookList App</h3>
                  <nav>
                     <ul className="main-navigation">
-                        <li>
-                            <a>Home</a>
-                        </li>
-                        <li>
-                            <a>About</a>
-                        </li>
-                        <li>
-                            <a>Contact</a>
-                        </li>
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Contact</a></li>
                     </ul>
                 </nav>
             </header>
         )
     }
-   
+}
+ --- */
+
+// 2) Accessing Context using Consumer. This approach can be used in functional components as well
+function Navbar() {
+    return (
+        <ThemeContext.Consumer>{(context)=>{
+            const {isLightTheme, dark, light} = context;
+            const theme = isLightTheme ? light : dark;
+            return(
+                <header style={{background:theme.ui, color:theme.text}}>
+                    <h3>BookList App</h3>
+                    <nav>
+                        <ul className="main-navigation">
+                            <li><a>Home</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Contact</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            )
+          }
+            
+        }
+        </ThemeContext.Consumer>
+        )
 }
 
 export default Navbar
