50dbac465cb43bda39ed23aebda31ae061bca265
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-19 12:35:27 +0530

    useContext() Hook to consume Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { ThemeContext } from '../contexts/ThemeContext'
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
+/* ---
 class BookList extends React.Component{
     static contextType = ThemeContext;
     render(){
@@ -26,4 +27,23 @@ class BookList extends React.Component{
     }
     
 }
+--- */
+
+// Will be using useContext() hook to consume the context
+
+const BookList = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div style={{ background: theme.bg, color: theme.text }}>
+      <ul className="booklist">
+        <li style={{ background: theme.ui }}>The Way of the Kings</li>
+
+        <li style={{ background: theme.ui }}>The Beautiful mind</li>
+
+        <li style={{ background: theme.ui }}>All that glitters is not gold</li>
+      </ul>
+    </div>
+  );
+};
 export default BookList;
